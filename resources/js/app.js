c909b87b3c78fb2c41115f536b5e232d96ff6bca
Maxang <maxsayr5@gmail.com>
2019-08-31 21:54:43 +0700

    (complete) video play trigger

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,21 +14,27 @@ var MasterStudio = {
     }
 }
 
-$('.activity-card > .video-wrapper').on('mouseenter', function () {
+$('.activity-card > .video-wrapper').off('mouseenter').on('mouseenter', function () {
     var self = this;
     $(this).addClass('hover');
     $(this).parent().children('.overlay').addClass('d-block');
-    $('.overlay.d-block').click(function () {
-        $(self).removeClass('hover');
-        $(this).removeClass('d-block');
-    })
-    $(this).click(function () {
+    $(this).off('click').on('click', function () {
+        console.log('play', MasterStudio.videoHover.play)
         if (!MasterStudio.videoHover.play)
             $(this).children('.video').get(0).play();
         else
             $(this).children('.video').get(0).pause();
         $(this).children('.play-wrapper').toggleClass('d-none');
         MasterStudio.videoHover.play = !MasterStudio.videoHover.play;
+    })
+
+    $('.overlay.d-block').one('click ', function () {
+        $(self).removeClass('hover');
+        $(this).removeClass('d-block');
+
+        $(self).children('.video').get(0).pause();
+        $(self).children('.play-wrapper').removeClass('d-none');
+        MasterStudio.videoHover.play = false;
     })
 })
 
