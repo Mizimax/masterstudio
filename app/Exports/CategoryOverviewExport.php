d4825446a8e1e9a57e4291d53ce348785ba4eaac
max <tammanoon@dek-d.com>
2020-05-12 04:05:36 +0700

    excel config file

---
+++ app/Exports/CategoryOverviewExport.php
@@ -0,0 +1,58 @@
+<?php
+
+	namespace App\Exports;
+
+	use App\Activity;
+	use App\ActivityStory;
+	use App\Category;
+	use App\Follow;
+	use App\Master;
+	use App\Studio;
+	use App\User;
+	use App\UserActivity;
+	use Illuminate\Contracts\View\View;
+	use Maatwebsite\Excel\Concerns\FromCollection;
+	use Maatwebsite\Excel\Concerns\FromView;
+
+	class CategoryOverviewExport implements FromView
+	{
+
+		public $categoryId;
+
+		public function __construct($categoryId)
+		{
+			$this->categoryId = $categoryId;
+		}
+
+		/**
+		 * @return \Illuminate\Support\Collection
+		 */
+		public function view(): View
+		{
+			$activityCount = Activity::where('category_id', $this->categoryId)->count();
+			$masterCount = Master::where('category_id', $this->categoryId)->count();
+			$studioCount = Studio::where('category_id', $this->categoryId)->count();
+			$storyCount = ActivityStory::join('activities as act', 'act.activity_id', 'activity_stories.activity_id')
+				->where('act.category_id', $this->categoryId)
+				->count();
+			$userActivityCount = UserActivity::from('user_activities as ua')
+				->join('activities as act', 'act.activity_id', 'ua.activity_id')
+				->where('act.category_id', $this->categoryId)
+				->count();
+			$followCount = Follow::join('users', 'follows.following_id', 'users.user_id')
+				->join('masters', 'masters.master_id', 'users.master_id')
+				->join('categories as cg', 'masters.category_id', 'cg.category_id')
+				->where('masters.category_id', $this->categoryId)->count();
+			$activity = Activity::where('category_id', $this->categoryId)
+				->select(\DB::raw('(activity_hour * activity_price) AS totalIncome'))->first();
+			return view('exports.overview-category', [
+				'activityCount' => $activityCount,
+				'userActivityCount' => $userActivityCount,
+				'storyCount' => $storyCount,
+				'followCount' => $followCount,
+				'totalIncome' => $activity['totalIncome'],
+				'masterCount' => $masterCount,
+				'studioCount' => $studioCount,
+			]);
+		}
+	}
---
+++ app/Exports/OverviewExport.php
@@ -0,0 +1,39 @@
+<?php
+
+	namespace App\Exports;
+
+	use App\Activity;
+	use App\Category;
+	use App\Follow;
+	use App\Master;
+	use App\User;
+	use App\UserActivity;
+	use Illuminate\Contracts\View\View;
+	use Maatwebsite\Excel\Concerns\FromView;
+
+	class OverviewExport implements FromView
+	{
+		/**
+		 * @return \Illuminate\Support\Collection
+		 */
+		public function view(): View
+		{
+			$allActivityCount = Activity::count();
+			$allMasterCount = Master::count();
+			$allUserCount = User::count();
+			$allUserActivityCount = UserActivity::count();
+			$allFollowCount = Follow::where('follow_type', 'studio')->count();
+			$allActivity = Activity::select(\DB::raw('(activity_hour * activity_price) AS totalIncome'))->first();
+			$categories = Category::get();
+			$result = [
+				'allActivityCount' => $allActivityCount,
+				'allMasterCount' => $allMasterCount,
+				'allUserActivityCount' => $allUserActivityCount,
+				'allUserCount' => $allUserCount,
+				'allFollowCount' => $allFollowCount,
+				'totalIncome' => $allActivity['totalIncome'],
+				'categories' => $categories
+			];
+			return view('exports.overview', $result);
+		}
+	}
